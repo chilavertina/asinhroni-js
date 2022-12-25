1c654a01af49e1c0e8a7e566bebd84080bfee93b
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-25 20:16:30 +0100

    stvaranje prvog ajax zahteva

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/serbia');
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText); //  this se odnosi na request
+});
