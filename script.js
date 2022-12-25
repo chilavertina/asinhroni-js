9c660a6818e481afb20300ceac1dc9d145edcc66
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-25 21:34:05 +0100

    callback hell - chaining ajax functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -39,3 +39,59 @@ const getCountryData = function (country) {
 getCountryData('portugal');
 getCountryData('serbia');
 getCountryData('russia');
+*/
+
+const renderCountry = function (data, className = '') {
+  const hmtl = ` <article class="${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${+(
+        data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', hmtl);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText); //  this se odnosi na request
+    // rezultat koda iznad se dobija u JSON-u
+
+    // pretvaranje JSON teksta u object
+    const [data] = JSON.parse(this.responseText); // [data] destruktuiranje niza sa objektom u object
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const [neighbour] = data.borders;
+
+    // const neighbour = data.borders?.[0]; // ovo je za slucaj da drzava nema suseda
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('serbia');
