4d011dedb2c9c53db290b7dfc549431998f79ad5
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-26 19:26:23 +0100

    AJAX call uz pomoc promises

--- script.js
+++ script.js
@@ -107,11 +107,40 @@ getCountryAndNeighbour('serbia');
 //   request.send();
 
 // NOVI nacin (promises)
+
+const renderCountry = function (data, className = '') {
+  const hmtl = ` <article class="${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${+(
+        data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', hmtl);
+  countriesContainer.style.opacity = 1;
+};
+
 const request = fetch('https://restcountries.com/v2/name/serbia');
 console.log(request);
 
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`).then(function (
+    // fetch funkcija return promise, a then metoda hendluje taj promise
     response
-  ) {});
+  ) {
+    console.log(response);
+    response.json().then(function (data) {
+      // da bismo zatim procitali podatak iz response-a koristimo json koji ce takodje vratiti promise
+      console.log(data); // opet na novi promise pozivamo then metodu i dobijamo pristup podatku
+      renderCountry(data[0]);
+    });
+  });
 };
+
+getCountryData('serbia');
