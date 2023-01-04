dd82cdfea99f35112e995f74cddca3d0fcb9cdc7
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-04 19:36:32 +0100

    ubacivanje tajmera u promise kao deo asinhronog JS

--- script.js
+++ script.js
@@ -238,11 +238,14 @@ console.log('Test end'); // 2.
 // BUILDING PROMISE
 
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) {
-    resolve('You WIN 💰');
-  } else {
-    reject('You lost your money 💩');
-  }
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
 });
 
 lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
