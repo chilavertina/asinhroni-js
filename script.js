a7f9654934c1e3a3444d8976d7fcf84cb2f00dc6
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-04 19:54:17 +0100

    primer promisifying-a

--- script.js
+++ script.js
@@ -263,3 +263,34 @@ wait(2)
     return wait(1);
   })
   .then(() => console.log('I waited for 1 second more'));
+
+/*
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+// resenje primera iznad na laksi nacin
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
