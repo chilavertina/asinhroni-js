3e0831b9fa5e8676f7a3934435c38d9b58a3dd52
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2023-01-08 12:54:38 +0100

    coding challenge 3

--- vezba.js
+++ vezba.js
@@ -60,7 +60,7 @@ whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 */
-
+/*
 // CODING CHALLENGE 2
 
 const imgContainer = document.querySelector('.images');
@@ -107,3 +107,62 @@ createImage('img/img-1.jpg')
     currentImg.style.display = 'none';
   })
   .catch(err => console.error(err));
+*/
+//  CODING CHALLENGE 3
+
+const imgContainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const newImage = document.createElement('img');
+    newImage.src = imgPath;
+
+    newImage.addEventListener('load', function () {
+      imgContainer.append(newImage);
+      resolve(newImage);
+    });
+
+    imgContainer.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    let newImage = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    newImage.style.display = 'none';
+
+    newImage = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    newImage.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    const imgEl = await Promise.all(imgs); // izvlaci elemente (img) iz Promise-a koji nastaju u okviru const imgs
+    console.log(imgEl);
+    imgEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
